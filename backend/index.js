44fa7c0543d7fb4787f044477521e6c5e4f71c59
Sophia Thomas <sophiart03@gmail.com>
2025-08-31 22:14:42 -0700

    Connect MongoDB to backend and add nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const nodemon = require("nodemon")
 
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+app.use(cors()); // allow all origins 
 app.use(express.json());
 
-// // Connect to MongoDB
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// .then(() => console.log("MongoDB connected"))
-// .catch(err => console.error("MongoDB connection error:", err));
+// Connect to MongoDB
+// const mongoUri = process.env.MONGODB_URI; 
+// // if (!mongoUri) {
+// //   throw new Error("MONGODB_URI is not defined in .env")
+// // }
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from backend + MongoDB!" });
+  res.json({ 
+    message: "Hello from Express backend!",
+    message2:"xoxoxoxoxoxo "
+  });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
